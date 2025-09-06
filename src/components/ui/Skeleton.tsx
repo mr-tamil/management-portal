87b89e3385e77aa9979ab6c2ebb875e3a7935aed
Mr. Tamil <mr.tamil003@gmail.com>
2025-09-06 13:03:26 +0000

    New Start

--- src/components/ui/Skeleton.tsx
+++
@@ -1,6 +0,0 @@
-const Skeleton = ({ className }: { className?: string }) => {
-  return (
-    <div className={`animate-pulse rounded-md bg-gray-700 ${className}`} />
-  );
-};
-export default Skeleton;
